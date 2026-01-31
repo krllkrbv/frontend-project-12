9894fd4f4504f069b234aa89a666164a6020f726
krllkrbv <krllkrbv@mail.ru>
2026-01-31 12:54:53 +0300

    fix main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,22 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './i18n'
-import './utils/rollbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react'
+
+const rollbarConfig = {
+  accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
+  environment: import.meta.env.MODE || 'test',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+	<RollbarProvider config={rollbarConfig}>
+		<ErrorBoundary>
+          <App />
+		</ErrorBoundary>
+	</RollbarProvider>
   </StrictMode>,
 )
--- frontend/src/utils/rollbar.js
+++
@@ -1,4 +0,0 @@
-export const rollbarConfig = {
-  accessToken: 'ed8789be6ecc4ad9b70a3a58655f8315',
-  environment: 'testenv',
-};
